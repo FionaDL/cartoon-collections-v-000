1c2353930fc289d17b1817ee750e526fcdda3a24
Fiona Lapham <fionadlapham@gmail.com>
2017-11-18 02:52:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,11 @@ def summon_captain_planet(array) #code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+  calls.any? do |call|
+    if call.lenght > 4 
+      true
+  end
 end
 
 def find_the_cheese# code an argument here
